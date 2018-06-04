a19f095b7a2adf824f1037f4a6c45248711a9b61
Cemal Biryol <cbbiryol@cemalsmbook1774.attlocal.net>
2018-06-04 11:51:18 -0400

    to be deployed in heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "bucketList_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: "",
+        database: "bucketList_db"
+    })
+}
 
-connection.connect(function(err){
-    if (err) {
-        console.error("error connecting to database:" + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-})
+connection.connect();
 
 //Export connection for ORM 
 module.exports = connection;
